957d5ede838747bdb08f92f7b67904b5c456292c
JLVega05 <jairon.vega2005@gmail.com>
2025-02-08 00:16:08 +0100

    Sobrescribiendo proyecto con nueva versión en Vite

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
+import ProductGrid from "./components/ProductGrid";
+import Footer from "./components/Footer";
+import ProductDetails from "./components/ProductDetails";
+import { Box, CssBaseline } from "@mui/material";
+
+const Success = () => <h1>Pagament completat!</h1>;
+const Cancel = () => <h1>El pagament s'ha cancel·lat.</h1>;
+
+const App = () => {
+  return (
+    <Router>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100vh",
+        }}
+      >
+        <CssBaseline />
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Banner />
+                <ProductGrid />
+              </>
+            }
+          />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/success" element={<Success />} />
+          <Route path="/cancel" element={<Cancel />} />
+        </Routes>
+        <Footer />
+      </Box>
+    </Router>
+  );
+};
+
+export default App;
